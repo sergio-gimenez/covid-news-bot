5cec4dfa495d048baf0455f30c3d13efbe4b695b
paucutrina <paucutrina@gmail.com>
2020-03-21 21:30:36 +0100

    Merge branch solve conflicts

--- database_connection.py
+++ database_connection.py
@@ -13,18 +13,32 @@ class MongoDB:
     def insert_one(self, dict_to_insert):
         self.news.insert_one(dict_to_insert)
 
-    def find_by_time(self, time_begin):
-        cursor = self.news.find({'inserted_at': {'$gte': time_begin}})
+    def find(self, filter, project={}):
+        cursor = self.news.find(filter, project)
         return [row for row in cursor]
 
     def find_by_keyword(self, keyword):
         cursor = self.news.find({'keyword': {'$regex': keyword, '$options':'i'}})
         return [row for row in cursor]
-if __name__=="__main__":
+
+    def find_by_time(self, time_begin):
+        filter = {'inserted_at': {'$gte': time_begin}}
+        return self.find(filter)
+
+    def update_category(self, category):
+        filter = {'category': {'$exists': False}}
+        list_to_update = self.find(filter)
+        for tweet_no_key in list_to_update:
+            id_tweet = {'id': tweet_no_key['id']}
+            to_add = {'$set': {'category': category}}
+            self.news.updateOne(id_tweet, to_add)
+            print('updated')
+
+
+
+if __name__ == "__main__":
     mongo = MongoDB()
     time = datetime(2014, 9, 24, 7, 51)
     algo = mongo.find_by_time(time)
-    algo2 = mongo.find_by_keyword('jordi')
-    print(algo2)
     print()
 
